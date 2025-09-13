6f74b5c3085384a4034d31c9cd6138a3c5766ec2
Jaxon Smith <jaxon.smith.8225@gmail.com>
2025-09-13 16:02:22 -0700

    connection to muse headband and analysis

---
+++ utils.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Muse LSL Example Auxiliary Tools
+
+These functions perform the lower-level operations involved in buffering,
+epoching, and transforming EEG data into frequency bands
+
+@author: Cassani
+"""
+
+import numpy as np
+from scipy.signal import butter, lfilter, lfilter_zi
+
+NOTCH_B, NOTCH_A = butter(4, np.array([55, 65]) / (256 / 2), btype='bandstop')
+
+def compute_band_powers(eegdata, fs):
+    """Extract the features (band powers) from the EEG.
+
+    Args:
+        eegdata (numpy.ndarray): array of dimension [number of samples,
+                number of channels]
+        fs (float): sampling frequency of eegdata
+
+    Returns:
+        (numpy.ndarray): feature matrix of shape [number of feature points,
+            number of different features]
+    """
+    # 1. Compute the PSD
+    winSampleLength, nbCh = eegdata.shape
+
+    # Apply Hamming window
+    w = np.hamming(winSampleLength)
+    dataWinCentered = eegdata - np.mean(eegdata, axis=0)  # Remove offset
+    dataWinCenteredHam = (dataWinCentered.T * w).T
+
+    NFFT = nextpow2(winSampleLength)
+    Y = np.fft.fft(dataWinCenteredHam, n=NFFT, axis=0) / winSampleLength
+    PSD = 2 * np.abs(Y[0:int(NFFT / 2), :])
+    f = fs / 2 * np.linspace(0, 1, int(NFFT / 2))
+
+    # SPECTRAL FEATURES
+    # Average of band powers
+    # Delta <4
+    ind_delta, = np.where(f < 4)
+    meanDelta = np.mean(PSD[ind_delta, :], axis=0)
+    # Theta 4-8
+    ind_theta, = np.where((f >= 4) & (f <= 8))
+    meanTheta = np.mean(PSD[ind_theta, :], axis=0)
+    # Alpha 8-12
+    ind_alpha, = np.where((f >= 8) & (f <= 12))
+    meanAlpha = np.mean(PSD[ind_alpha, :], axis=0)
+    # Beta 12-30
+    ind_beta, = np.where((f >= 12) & (f < 30))
+    meanBeta = np.mean(PSD[ind_beta, :], axis=0)
+
+    feature_vector = np.concatenate((meanDelta, meanTheta, meanAlpha,
+                                     meanBeta), axis=0)
+
+    feature_vector = np.log10(feature_vector)
+
+    return feature_vector
+
+def nextpow2(i):
+    """
+    Find the next power of 2 for number i
+    """
+    n = 1
+    while n < i:
+        n *= 2
+    return n
+
+def update_buffer(data_buffer, new_data, notch=False, filter_state=None):
+    """
+    Concatenates "new_data" into "data_buffer", and returns an array with
+    the same size as "data_buffer"
+    """
+    if new_data.ndim == 1:
+        new_data = new_data.reshape(-1, data_buffer.shape[1])
+
+    if notch:
+        if filter_state is None:
+            filter_state = np.tile(lfilter_zi(NOTCH_B, NOTCH_A),
+                                   (data_buffer.shape[1], 1)).T
+        new_data, filter_state = lfilter(NOTCH_B, NOTCH_A, new_data, axis=0,
+                                         zi=filter_state)
+
+    new_buffer = np.concatenate((data_buffer, new_data), axis=0)
+    new_buffer = new_buffer[new_data.shape[0]:, :]
+
+    return new_buffer, filter_state
+
+def get_last_data(data_buffer, newest_samples):
+    """
+    Obtains from "buffer_array" the "newest samples" (N rows from the
+    bottom of the buffer)
+    """
+    new_buffer = data_buffer[(data_buffer.shape[0] - newest_samples):, :]
+
+    return new_buffer
