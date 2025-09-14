265eea860c3fdc4e7c6a2ae440ff4a2525034573
dschlesinger <denalischlesinger06@gmail.com>
2025-09-13 21:33:49 -0400

    Clean up Suno

--- suno_utils/stream_audio.py
+++ suno_utils/stream_audio.py
@@ -1,14 +1,31 @@
 import vlc
 import time
+from dataclasses import dataclass
+import threading
 
-def stream_audio(url):
-    player = vlc.MediaPlayer(url)
-    player.play()
+class MediaPlayer:
     
-    # Wait for it to start
-    while player.get_state() not in [vlc.State.Playing, vlc.State.Ended]:
-        time.sleep(0.1)
-    
-    # Wait for it to finish
-    while player.get_state() != vlc.State.Ended:
-        time.sleep(1)
+    url: str
+
+    def __init__(self, url: str) -> None:
+
+        self.url = url
+        self.player = vlc.MediaPlayer(url)
+
+    def play(self) -> None:
+
+        self.player_thread = threading.Thread(target=lambda: self.player.play())
+
+        self.player.play()
+
+        # Wait until playing
+        while self.player.get_state() not in [vlc.State.Playing, vlc.State.Ended]:
+            time.sleep(0.1)
+
+    def time_left(self) -> float:
+
+        return self.player.duration() * (1 - self.player.get_position())
+
+    def is_playing(self) -> bool:
+
+        return self.player.get_state() == vlc.State.Playing
